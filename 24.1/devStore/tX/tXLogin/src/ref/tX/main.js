419d7390fd1b8c24f4aa45d4404d12a27933f008
techist007 <techist007@gmail.com>
2024-12-06 02:35:00 +0530

    24.1.3: added login screen high level skeleton
    24.1.3: added login screen high level skeleton

--- 24.1/devStore/tX/tXLogin/src/ref/tX/main.js
+++ 24.1/devStore/tX/tXLogin/src/ref/tX/main.js
@@ -1,11 +1,15 @@
-import tX from '../../../../ref/tX.24.1.1.dev.min.js';
+import tX from '../../../../ref/tX.24.1.2.dev.min.js';
 export default class comp { }
 
 (async () => {
-    
+
     //#region add the config entries here.  Please see the sample entry provided below
     comp.config = class {
         static deleteThis = 'delete this config entry.  not for production.  please use the config entries as per the development.';
+        static tXLoginScreen = 'tXLoginScreen';
+        static tXCenterDiv = 'tXCenterDiv';
+        static tXCenterContent = 'tXCenterContent';
+        static tXScreenMessage = 'tXScreenMessage';
     }
     //#endregion add the config entries here.  Please see the sample entry provided below
 
@@ -13,15 +17,16 @@ export default class comp { }
     comp.messages = class {
         static deleteThis = 'delete this message.  not for production.  please use the messages as per the development.';
     }
-    
+
     //#endregion add the messages here.  Please see the sample entry provided below
     comp.tXLogin = class {
-    
+
         //#region mobile
-        #postMobileAsync = async() => {
-            return await new tX.el().postAsync('body', {
+        #postMobileAsync = async (loginEl) => {
+            let loginElId = loginEl === document.body ? 'body' : loginEl.id;
+            return await new tX.el().postAsync(loginElId, {
                 'elementType': 'h3'
-                , 'elementId': 'comp'
+                , 'elementId': `${loginElId}-root`
                 , 'text': 'Your mobile login goes here'
                 , 'classList': [], 'style': {}
             });
@@ -29,10 +34,11 @@ export default class comp { }
         //#endregion mobile
 
         //#region tablet
-        #postTabletAsync = async() => {
-            return await new tX.el().postAsync('body', {
+        #postTabletAsync = async (loginEl) => {
+            let loginElId = loginEl === document.body ? 'body' : loginEl.id;
+            return await new tX.el().postAsync(loginElId, {
                 'elementType': 'h3'
-                , 'elementId': 'comp'
+                , 'elementId': `${loginElId}-root`
                 , 'text': 'Your tablet login goes here'
                 , 'classList': [], 'style': {}
             });
@@ -40,30 +46,91 @@ export default class comp { }
         //#endregion tablet
 
         //#region desktop
-        #postDesktopAsync = async() => {
-            return await new tX.el().postAsync('body', {
-                'elementType': 'h3'
-                , 'elementId': 'comp'
-                , 'text': 'Your desktop login goes here'
-                , 'classList': [], 'style': {}
+        #postDesktopCSSAsync = async (id) => {
+            let css = ''
+                + '.tXLoginScreen { background: #eeecec; }'
+                + '.tXCenterDiv { position: relative; height: 500px; }'
+                + '.tXCenterContent { position: absolute; margin: 0; top: 50%; left: 50%; transform: translate(-50%, -50%); }'
+                + '.tXScreenMessage { background: #e5dada; padding: 10px; }'
+                ;
+            await tX.css.postCSSAsync(id, css);
+        }
+        #postDesktopMessageAsync = async (loginMessageListEl) => {
+            // post desktop message here
+            await new tX.el().postAsync(loginMessageListEl.id, {
+                'elementType': 'div'
+                , 'text': 'All the messages here.  But this is message 1, for example.'
+                , 'elementId': `${loginMessageListEl.id}-message1`
+                , 'classList': [ comp.config.tXScreenMessage ], 'style': {}
             });
+        }
+        #postDesktopScreenAsync = async (loginScreenEl) => {
+            // post desktop screen here
+            await new tX.el().postAsync(loginScreenEl.id, {
+                'elementType': 'div'
+                , 'elementId': `${loginScreenEl.id}-content`
+                , 'classList': [comp.config.tXCenterDiv], 'style': {}
+            }).then(async tXContentEl => {
+                await new tX.el().postAsync(tXContentEl.id, {
+                    'elementType': 'div'
+                    , 'elementId': `${tXContentEl.id}-content`
+                    , 'classList': [comp.config.tXCenterContent], 'style': {}
+                }).then(async tXBodyEl => {
+                    await new tX.el().postAsync(tXBodyEl.id, {
+                        'elementType': 'p'                        
+                        , 'elementId': `${tXBodyEl.id}-section`
+                        , 'classList': [], 'style': {}
+                    }).then(async el => {
+                        el.innerHTML = 'Actual login screen goes here.'
+                        + '<br>All the elements should be displayed in the center of the screen like this.'
+                        + '<br>Please use the <strong>comp.config.tXCenterDiv</strong> & <strong>comp.config.tXCenterContent</strong> css classes to center the elements.';
+                    });
+                });
+            });
+        }
+        #postDesktopAsync = async (loginEl) => {
+            // get the login screen parent id
+            let loginElId = loginEl === document.body ? 'body' : loginEl.id;
+
+            // post your desktop css here
+            await this.#postDesktopCSSAsync(`${loginElId}-tXLogin-desktop-css`);
+
+            return await new tX.el().postAsync(loginElId, {
+                'elementType': 'div'
+                , 'elementId': `${loginElId}-login-root`
+                , 'classList': [comp.config.tXLoginScreen], 'style': {}
+            })
+                .then(async loginRootEl => {
+                    await new tX.el().postAsync(loginRootEl.id, {
+                        'elementType': 'div'
+                        , 'elementId': `${loginRootEl.id}-message`
+                        , 'classList': [], 'style': {}
+                    })
+                        .then(async loginMessageListEl => { await this.#postDesktopMessageAsync(loginMessageListEl); });
+                    await new tX.el().postAsync(loginRootEl.id, {
+                        'elementType': 'div'
+                        , 'elementId': `${loginRootEl.id}-screen`
+                        , 'classList': [], 'style': {}
+                    })
+                        .then(async loginScreenEl => { await this.#postDesktopScreenAsync(loginScreenEl); });
+                });
         }
         //#endregion desktop
 
         //#region component crud
-        postAsync = async() => {
+        postAsync = async (loginEl) => {
             let deviceType = await tX.device.getAsync();
             if (deviceType === tX.config.mobile) {
-               return await this.#postMobileAsync(); 
+                return await this.#postMobileAsync(loginEl);
             }
             if (deviceType === tX.config.tablet) {
-                return await this.#postTabletAsync(); 
+                return await this.#postTabletAsync(loginEl);
             }
             if (deviceType === tX.config.desktop) {
-                return await this.#postDesktopAsync(); 
-            }            
+                return await this.#postDesktopAsync(loginEl);
+            }
         }
         //#endregion component crud
     }
-    return await new comp.tXLogin().postAsync().then(async compEl => {});
+    return await new comp.tXLogin().postAsync(document.body).then(async compEl => { });
 })();
